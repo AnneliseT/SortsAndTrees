593c533c8bb6f2e852d0fab507be5ebcbcf86551
Annelise Tsueda <annelisetsueda@annelises-macbook-air.local>
2016-09-05 18:27:38 -0500

    Initial Commit

---
+++ CPP_Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP_Practice
+//
+//  Created by Annelise Tsueda on 9/5/16.
+//  Copyright © 2016 Annelise Tsueda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
